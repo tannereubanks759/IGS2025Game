e6d55ac083e7603a27fb9246e41b29ccb87e7be3
Matthew Bolger <165869410+mattBolger@users.noreply.github.com>
2025-01-26 08:45:56 -0500

    Zombie AI
    Zombie moves towards player, stops within attacking range, and either attacks or moves within range again

--- Assets/Scripts/zombieAIV1.cs
+++ Assets/Scripts/zombieAIV1.cs
@@ -4,13 +4,16 @@ using UnityEngine.AI;
 public class zombieAIV1 : MonoBehaviour
 {
     // The player
-    [SerializeField] GameObject player = null;
+    GameObject player = null;
 
     // The navmesh agent controlling the zombie ai
     NavMeshAgent agent;
 
     // The animator attached to the zombie
     Animator animator;
+
+    // The number that controls the melee range of the ai
+    float attackRange = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,23 +25,62 @@ public class zombieAIV1 : MonoBehaviour
         animator = GetComponent<Animator>();
 
         // Get the player
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //// The zombie has a valid player to target
-        //if (player != null)
-        //{
-        //    // set the agent's destination to be the player's position
-        //    agent.destination = player.transform.position;
-        //}
+        PlayerDetected();
+        MoveAI();
+        CanAttack();
     }
 
     // Set the is standing bool to true
     public void IsStanding()
     {
         animator.SetBool("isStanding", true);
+    }
+
+    // Sets the animator bool for player detection
+    void PlayerDetected()
+    {
+        if (player != null)
+        {
+            // Set the animator bool to true so that the run anim plays
+            animator.SetBool("playerDetected", true);
+        }
+
+        else
+        {
+            // Set the animator bool to false (should only be on player death)
+            animator.SetBool("playerDetected", false);
+        }
+    }
+
+    // Handles AI movement
+    void MoveAI()
+    {
+        if (animator.GetBool("playerDetected") && animator.GetBool("isStanding") && !animator.GetBool("canAttack"))
+        {
+            // set the agent's destination to be the player's position
+            agent.destination = player.transform.position;
+        }
+    }
+
+    // Checks if the AI can attack
+    void CanAttack()
+    {
+        
+        if ((player.transform.position - this.transform.position).magnitude < attackRange)
+        {
+            agent.isStopped = true;
+            animator.SetBool("canAttack", true);
+        }
+        else
+        {
+            agent.isStopped = false;
+            animator.SetBool("canAttack", false);
+        }
     }
 }
