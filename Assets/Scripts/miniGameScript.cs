37e5dcb4b8a741eb052fc1a65af1beda5441eac1
Andrewk57 <112024432+Andrewk57@users.noreply.github.com>
2025-01-25 23:07:54 -0500

    more minigame stuff

--- Assets/Scripts/miniGameScript.cs
+++ Assets/Scripts/miniGameScript.cs
@@ -1,26 +1,39 @@
+using Unity.AppUI.UI;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class miniGameScript : MonoBehaviour
 {
     public GameObject ui;
+    public bool isInteractable = false;
+    private bool hasQuest = false;
+    public string quest;
+    public string[] quests = { "Get 8 headshots", "Get kills within indicated zone", "Get 5 kills without reloading " };
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ui.SetActive(false);
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKeyDown(KeyCode.E) && isInteractable && hasQuest == false)
+        {
+            startMinigame();
+        }
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            hasQuest = false;
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && hasQuest == false)
         {
             Debug.Log("Entered zone");
             ui.SetActive(true);
+            isInteractable=true;
         }
     }
     private void OnCollisionExit(Collision collision)
@@ -29,6 +42,18 @@ public class miniGameScript : MonoBehaviour
         {
             Debug.Log("Left Zone");
             ui.SetActive(false);
+            isInteractable=false;
         }
     }
+    void startMinigame()
+    {
+        //float gameNumber = Random.value;
+        
+        int number = Random.Range( 0,  quests.Length);
+        Debug.Log(number);
+        hasQuest = true;
+        quest = quests[number];
+        Debug.Log(quest);
+    }
+    
 }
