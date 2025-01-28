eb20b2657ab66202ec162e7d1409e152cedd3f76
Andrewk57 <112024432+Andrewk57@users.noreply.github.com>
2025-01-28 16:42:35 -0500

    swing trap added

---
+++ Assets/Scripts/swingTrap.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class swingTrap : MonoBehaviour
+{
+    //public Animator animator;
+    public Animator trapAnim;
+    bool canInteract = false;
+    public GameObject interactUI;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        interactUI.SetActive(false);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //debug key
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            trapAnim.SetBool("isOn", true);
+        }
+        if (Input.GetKeyDown(KeyCode.E)&& canInteract) 
+        {
+            trapAnim.SetBool("isOn", true);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        canInteract = true;
+        interactUI.SetActive(true);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        canInteract = false;
+        interactUI.SetActive(false);
+    }
+}
