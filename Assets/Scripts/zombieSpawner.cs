d3541cfa3c8fe301f047d24d954074dc72bcce12
Matthew Bolger <165869410+mattBolger@users.noreply.github.com>
2025-02-01 21:15:38 -0500

    Zombie Spawner
    Spawner activates based on player distance,
    spawns a random zombie from the array of prefabs

--- Assets/Scripts/zombieSpawner.cs
+++ Assets/Scripts/zombieSpawner.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 public class zombieSpawner : MonoBehaviour
 {
     // The zombie prefabs
-    [SerializeField] GameObject[] zombies;
+    [SerializeField] private GameObject[] zombies;
 
     // The game time
     private float gameTime;
 
+    [SerializeField] private float spawnRange;
+
+    // Player tracking
     private bool playerNear;
+    private GameObject player;
+    private float playerDistance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
+        IsPlayerNear();
+        GameTimeManager();
     }
 
     // Time calculations
@@ -26,10 +33,36 @@ public class zombieSpawner : MonoBehaviour
     {
         gameTime += Time.deltaTime;
 
-        if (gameTime > 4 && playerNear)
+        if (gameTime > 5 && playerNear)
         {
-
+            SpawnZombie(zombies);
         }
     }
 
+    // Spawns a random zombie from the array, resets the game time
+    void SpawnZombie(GameObject[] zombArray)
+    {
+        var index = UnityEngine.Random.Range(0, zombArray.Length);
+
+        GameObject Zombie = Instantiate(zombArray[index], this.transform.position, Quaternion.identity);
+
+        gameTime = 0;
+    }
+
+    // Checks if the player is near
+    void IsPlayerNear()
+    {
+        // calculate player distance
+        playerDistance = (player.transform.position - this.transform.position).magnitude;
+
+        // set variables
+        if (playerDistance > spawnRange)
+        {
+            playerNear = false;
+        }
+        else
+        {
+            playerNear = true;
+        }
+    }
 }
